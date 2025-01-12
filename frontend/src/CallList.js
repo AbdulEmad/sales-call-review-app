557834eeb14f58314b6caf15bd674a0be280cbd0
abdul <abdul.emad0@gmail.com>
2025-01-12 15:52:42 +0000

    envs

--- frontend/src/CallList.js
+++ frontend/src/CallList.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 
 function CallList({ onSelectCall }) {
   const [calls, setCalls] = useState([]);
+  const backendApiUrl = process.env.REACT_APP_BACKEND_API_URL || "http://localhost:8000";
 
   useEffect(() => {
-    fetch(`${process.env.BACKEND_API_URL}/calls`)
+    fetch(`${backendApiUrl}/calls`)
       .then((res) => res.json())
       .then((data) => setCalls(data))
       .catch((err) => console.error("Error fetching calls:", err));
-  }, []);
+  }, [backendApiUrl]);
 
   return (
     <div style={{ width: "300px" }}>
